6c533750629c5ef7ed5a8710e1170623590e8a24
w3zeblamf <zeblamfw3@gmail.com>
2022-04-11 17:57:27 -0400

    files updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //App Container
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        className='freecodecamp-logo' 
+
+        src={freeCodeCampLogo}
+        
+        alt='Logo de freeCodeCamp'/>
+
+      </div>
+      <div className='contenedor-principal'>
+        <Boton />
+        <Boton />
+
+      </div>
+      
     </div>
+    //End of App Container
   );
 }
 
---
+++ src/componentes/Boton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Boton({ texto, esBotonDeClic, manejarClic }) {
+  return (
+    <button
+    
+    className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+    onClick={manejarClic}>
+      {texto}
+    </button>
+
+  );
+}
+
+export default Boton;
+
+//En React-JS comúnmente se trabaja con la sintaxis de des-estructuración
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
